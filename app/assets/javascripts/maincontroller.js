6feb1cb0bfa7e5eb8c66088b513f13ae1f33b962
Anthony Corrado <amcstudios@yahoo.com>
2014-07-16 12:09:00 -0700

    refirst commit

---
+++ app/assets/javascripts/maincontroller.js
@@ -0,0 +1,4 @@
+app.controller("MainController", function($scope) {
+	$scope.squares = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+	$scope.navItems = ["Expertise", "Education", "About Me", "Projects", "Contact"];
+});
